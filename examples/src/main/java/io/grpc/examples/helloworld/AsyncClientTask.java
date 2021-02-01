5d9fd5488b6acad541686829667bd6300726a0e6
tk <123>
2021-02-01 18:21:58 +0800

    async

---
+++ examples/src/main/java/io/grpc/examples/helloworld/AsyncClientTask.java
@@ -0,0 +1,12 @@
+package io.grpc.examples.helloworld;
+
+/**
+ * @author: tk
+ * @since: 2021/2/1
+ */
+public class AsyncClientTask implements Runnable{
+    @Override
+    public void run() {
+
+    }
+}
